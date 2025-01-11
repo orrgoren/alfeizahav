799cc51f6e89215c7bc5421df4cbe6d7bb222ee0
Orr Goren <orrgorenn@gmail.com>
2025-01-11 22:41:50 +0200

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,26 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
-import type { Metadata } from "next";
-import { Rubik } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "@/components/navbar";
+import { GoogleAnalytics } from '@next/third-parties/google';
+import type { Metadata } from 'next';
+import { Rubik } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import Navbar from '@/components/navbar';
+import { Analytics } from '@vercel/analytics/react';
 
 const rubik = Rubik({
-  variable: "--font-rubik",
-  subsets: ["latin"],
+  variable: '--font-rubik',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "אלפיזהב - בלוג השקעות, טכנולוגיה וחיים",
-  description: "אלפיזהב - בלוג השקעות, טכנולוגיה וחיים",
-  robots: "index, follow",
+  title: 'אלפיזהב - בלוג השקעות, טכנולוגיה וחיים',
+  description: 'אלפיזהב - בלוג השקעות, טכנולוגיה וחיים',
+  robots: 'index, follow',
   keywords:
-    "בלוג, אלפיזהב, השקעות, טכנולוגיה, blog, alfeizahav, tech, technology, investments, stock market",
+    'בלוג, אלפיזהב, השקעות, טכנולוגיה, blog, alfeizahav, tech, technology, investments, stock market',
   openGraph: {
-    siteName: "אלפיזהב - בלוג השקעות, טכנולוגיה וחיים",
-    locale: "he",
-    type: "website",
+    siteName: 'אלפיזהב - בלוג השקעות, טכנולוגיה וחיים',
+    locale: 'he',
+    type: 'website',
   },
 };
 
@@ -38,12 +39,12 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navbar />
-          <main className="max-w-4xl mx-auto px-4">{children}</main>
-          <footer className="my-4 flex items-center justify-center w-full font-semibold text-sm text-gray-500 z-11">
-            &copy; 2025 אלפי<span className="text-primary">זהב</span>. כל
-            הזכויות שמורות.
+          <main className="mx-auto max-w-4xl px-4">{children}</main>
+          <footer className="z-11 my-4 flex w-full items-center justify-center text-sm font-semibold text-gray-500">
+            &copy; 2025 אלפי<span className="text-primary">זהב</span>. כל הזכויות שמורות.
           </footer>
         </ThemeProvider>
+        <Analytics />
       </body>
       <GoogleAnalytics gaId="G-5QBNK188V9" />
     </html>
